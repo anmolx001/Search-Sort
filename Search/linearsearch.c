5b403eb42b8356f7e4b88e2cb87daa562b0c9449
anmolx001 <56028445+anmolx001@users.noreply.github.com>
2020-01-31 17:10:43 +0530

    Update linearsearch.c

--- Search/linearsearch.c
+++ Search/linearsearch.c
@@ -1,1 +1,44 @@
 
+
+#include <stdio.h>
+ 
+#define MAX 5
+ 
+int linearSearch(int *a,int n)
+{
+    int i,pos=-1;
+ 
+    for(i=0;i< MAX; i++)
+    {
+        if(a[i]==n)
+        {
+            pos=i;
+            break;
+        }
+    }
+    return pos;
+}
+ 
+int main()
+{
+    int i,n,arr[MAX];
+    int num;     
+    int position;
+ 
+    printf("\nEnter array elements:\n");
+    for(i=0;i< MAX;i++)
+        scanf("%d",&arr[i]);
+ 
+    printf("\nNow enter element to search :");
+    scanf("%d",&num);
+ 
+    
+    position=linearSearch(arr,num);
+ 
+    if(num==-1)
+        printf("Element not found.\n");
+    else
+        printf("Element found @ %d position.\n",position);
+ 
+    return 0;
+}
